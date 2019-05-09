ed292dbb3f790db86a7222bb437cdae20b1f1977
NCKLA <nckla000@users.noreply.github.com>
2019-05-09 21:11:36 +0800

    update 19.5.9

--- policy learning(acc use action)/new_metric_fn_demo.py
+++ policy learning(acc use action)/new_metric_fn_demo.py
@@ -1,3 +1,4 @@
+# @register_metric('pre_action_accuracy')
 def pre_action_accuracy(y_true, y_predict):
     # 进来的数据是list   里面包含4个list  这四个list里面是一整段对话  改了代码之后y_predict应该输入的是
     y_true = [y['act'] for dialog in y_true for y in dialog]
@@ -11,7 +12,38 @@ def pre_action_accuracy(y_true, y_predict):
     correct = sum([y1.strip().lower() == y2.strip().lower() for y1, y2 in zip(y_true, y_predicted)])
     return correct / examples_len if examples_len else 0
 
+
+# @register_metric('per_item_dialog_bleu_kai')
+
+# bleu use dialog sentence , not the action
+def per_item_dialog_bleu_kai(y_true, y_predicted):
+    from nltk.translate.bleu_score import corpus_bleu
+    y_true = (y['text'] for dialog in y_true for y in dialog)
+    return corpus_bleu([[y_t.lower().split()] for y_t in y_true],
+                       [y_q.lower().split() for y_p in y_predicted for y_q in y_p])
+
 # TODO：需要添加到记事本metrics_registry  可能还要写 @ 这个东西    明天就去改输出为action，记得注释写好的（4.26）
+
+
+action_list = ['api_call', 'bye', 'canthear', 'canthelp_area', 'canthelp_area_food', 'canthelp_area_food_pricerange',
+               'canthelp_area_pricerange', 'canthelp_food', 'canthelp_food_pricerange', 'confirm-domain',
+               'expl-conf_area', 'expl-conf_food', 'expl-conf_pricerange',
+               'impl-conf_area+impl-conf_pricerange+request_food', 'impl-conf_food+impl-conf_pricerange+request_area',
+               'impl-conf_food+request_area', 'inform_addr+inform_food+offer_name',
+               'inform_addr+inform_phone+inform_pricerange+offer_name', 'inform_addr+inform_phone+offer_name',
+               'inform_addr+inform_postcode+offer_name', 'inform_addr+inform_pricerange+offer_name',
+               'inform_addr+offer_name', 'inform_area+inform_food+inform_pricerange+offer_name',
+               'inform_area+inform_food+offer_name', 'inform_area+inform_phone+offer_name',
+               'inform_area+inform_postcode+offer_name', 'inform_area+inform_pricerange+offer_name',
+               'inform_area+offer_name', 'inform_food+inform_pricerange+offer_name',
+               'inform_food+offer_name', 'inform_phone+inform_postcode+offer_name',
+               'inform_phone+inform_pricerange+offer_name', 'inform_phone+offer_name',
+               'inform_postcode+inform_pricerange+offer_name', 'inform_postcode+offer_name',
+               'inform_pricerange+offer_name', 'offer_name', 'repeat', 'reqmore', 'request_area', 'request_food',
+               'request_pricerange', 'select_area', 'select_food', 'select_pricerange', 'welcomemsg']
+# 在bot那里  把这个函数改成这样  当然  前面要自己声明一个action_list
+#     def _decode_response(self, action_id):
+#         return action_list[int(action_id)]
 
 # 下面惯例先test一下
 # test = {"y": [1, 2, 3, 4, 5], "y_pre": [[6], [7, 8], 9, 0, "test"]}
